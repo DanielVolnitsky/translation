0775972de9801bfc91425ca329df7a9f6920cc66
Spok <game1life1@gmail.com>
2020-06-17 23:00:24 +0300

    Rework logger, add winston for log file output

--- services/src/app.controller.ts
+++ services/src/app.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Get } from '@nestjs/common'
+import { Controller, Get, Logger } from '@nestjs/common'
 import { AppService } from './app.service'
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+  private static readonly logger = new Logger(AppController.name)
 
   @Get()
   getHello(): string {
+    AppController.logger.log('Get request to / route', AppController.name)
     return this.appService.getHello()
   }
 }
--- services/src/main.ts
+++ services/src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core'
+import { Logger } from '@nestjs/common'
+import * as winston from 'winston'
+import { WinstonModule } from 'nest-winston'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
-  await app.listen(3000)
+  const logger = new Logger('bootstrap')
+  const app = await NestFactory.create(AppModule, {
+    logger: WinstonModule.createLogger({
+      transports: [
+        new winston.transports.File({ filename: 'info.log', level: 'info' }),
+        new winston.transports.File({ filename: 'error.log', level: 'error' })
+      ]
+    })
+  })
+
+  const port = 3000 // TODO: do it process.env variable
+  await app.listen(port)
+  logger.log(`Application listening on port ${port}`)
 }
 bootstrap()
