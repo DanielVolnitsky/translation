e3a2a50a6403a3997f82dcc19b86978276ebe86f
Spok <game1life1@gmail.com>
2020-06-19 20:26:33 +0300

    Add formatter to logger, add env variable

--- services/src/main.ts
+++ services/src/main.ts
@@ -2,21 +2,28 @@ import { NestFactory } from '@nestjs/core'
 import { Logger } from '@nestjs/common'
 import { transports } from 'winston'
 import { WinstonModule } from 'nest-winston'
+import { config } from 'dotenv'
 import { AppModule } from './app.module'
+
+import { logFormat } from './utils/LogFormat'
+
+config()
+const port = process.env.PORT || 3000
 
 async function bootstrap() {
   const logger = new Logger('bootstrap')
   const app = await NestFactory.create(AppModule, {
     logger: WinstonModule.createLogger({
+      format: logFormat,
       transports: [
         new transports.File({ filename: 'info.log', level: 'info' }),
-        new transports.File({ filename: 'error.log', level: 'error' })
+        new transports.File({ filename: 'error.log', level: 'error' }),
+        new transports.Console()
       ]
     })
   })
 
-  const port = 3000 // TODO: do it process.env variable
   await app.listen(port)
-  logger.log(`Application listening on port ${port}`)
+  logger.log(`Application listening on http://localhost:${port}`)
 }
 bootstrap()
