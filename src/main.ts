a2e6aad9fc55650c66761afd4d93c8d42226edfb
Spok <game1life1@gmail.com>
2020-06-21 01:50:28 +0300

    Logger improvements, remove dotenv deps, fix package json

--- src/main.ts
+++ src/main.ts
@@ -2,23 +2,21 @@ import { NestFactory } from '@nestjs/core'
 import { Logger } from '@nestjs/common'
 import { transports } from 'winston'
 import { WinstonModule } from 'nest-winston'
-import { config } from 'dotenv'
 import { AppModule } from './app.module'
 
-import { logFormat } from './utils/LogFormat'
+import { logFormatFile, logFormatConsole } from './utils/LogFormat'
 
-config()
 const port = process.env.PORT || 3000
 
 async function bootstrap() {
   const logger = new Logger('bootstrap')
   const app = await NestFactory.create(AppModule, {
     logger: WinstonModule.createLogger({
-      format: logFormat,
+      format: logFormatFile,
       transports: [
         new transports.File({ filename: 'info.log', level: 'info' }),
         new transports.File({ filename: 'error.log', level: 'error' }),
-        new transports.Console()
+        new transports.Console({ format: logFormatConsole })
       ]
     })
   })
