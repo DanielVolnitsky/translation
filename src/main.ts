62812bb8e1eedc1ba3f2e2343c7e203979ea8843
Spok <game1life1@gmail.com>
2020-06-23 22:36:43 +0300

    Refactor bootstrap function

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,11 @@
 import { NestFactory } from '@nestjs/core'
 import { Logger } from '@nestjs/common'
-import { transports } from 'winston'
-import { WinstonModule } from 'nest-winston'
 import { AppModule } from './app.module'
+import { appConfig, port } from './config/app.config'
 
-import { logFormatFile, logFormatConsole } from './utils/LogFormat'
-
-const port = process.env.PORT || 3000
-
-async function bootstrap() {
+async function bootstrap(): Promise<void> {
   const logger = new Logger('bootstrap')
-  const app = await NestFactory.create(AppModule, {
-    logger: WinstonModule.createLogger({
-      format: logFormatFile,
-      transports: [
-        new transports.File({ filename: 'info.log', level: 'info' }),
-        new transports.File({ filename: 'error.log', level: 'error' }),
-        new transports.Console({ format: logFormatConsole })
-      ]
-    })
-  })
+  const app = await NestFactory.create(AppModule, appConfig)
 
   await app.listen(port)
   logger.log(`Application listening on ${port}`)
