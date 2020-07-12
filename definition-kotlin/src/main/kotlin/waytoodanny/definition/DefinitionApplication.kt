9293d4b99f8134e03485cdb021d754e5f5747937
Daniel <waytodanny@gmail.com>
2020-07-12 18:27:37 +0300

    add kotlin-based definition backbone

---
+++ definition-kotlin/src/main/kotlin/waytoodanny/definition/DefinitionApplication.kt
@@ -0,0 +1,11 @@
+package waytoodanny.definition
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DefinitionApplication
+
+fun main(args: Array<String>) {
+    runApplication<DefinitionApplication>(*args)
+}
---
+++ definition-kotlin/src/test/kotlin/waytoodanny/definition/DefinitionApplicationTests.kt
@@ -0,0 +1,13 @@
+package waytoodanny.definition
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DefinitionApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
